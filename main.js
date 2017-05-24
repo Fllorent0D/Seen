10ab4a6067a83e036404707c70818adba1f730f9
Florent Cardoen <florent.cardoen@student.hepl.be>
2017-05-24 13:04:42 +0200

    beautifier js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain;
-const fs    = require('fs'),
+const fs = require('fs'),
     nconf = require('nconf');
 const path = require('path')
 const url = require('url')
@@ -16,47 +16,57 @@ const trakt = new Trakt({
 });
 const didYouMean = require('didyoumean2');
 
-nconf.file({ file: path.join(__dirname, 'token.json') });
+nconf.file({
+    file: path.join(__dirname, 'token.json')
+});
 let mainWindow, loginwindows
 
-function createWindow () {
-    mainWindow = new BrowserWindow({width:800, height:600, maxWidth:992, show:false, titleBarStyle: "hidden-inset"});//
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        maxWidth: 992,
+        show: false,
+        titleBarStyle: "hidden-inset"
+    }); //
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, '/renderer/index_mat.html'),
         protocol: 'file:',
         slashes: true
     }));
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
-  mainWindow.once("ready-to-show", ()=>{
-    mainWindow.show();
-    if(typeof nconf.get('TOKEN') !== 'undefined'){
-        trakt.import_token(nconf.get("TOKEN")).then(newTokens => {
-            console.log(newTokens);
-            mainWindow.webContents.send("feedback", {text:"Already logged in"});
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+    mainWindow.once("ready-to-show", () => {
+        mainWindow.show();
+        if (typeof nconf.get('TOKEN') !== 'undefined') {
+            trakt.import_token(nconf.get("TOKEN")).then(newTokens => {
+                console.log(newTokens);
+                mainWindow.webContents.send("feedback", {
+                    text: "Already logged in"
+                });
 
-            // Contains token, refreshed if needed (store it back)
-        });
-    }
-  })
+                // Contains token, refreshed if needed (store it back)
+            });
+        }
+    })
 
 
 }
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 ipc.on("code", (event, arg) => {
     console.log(arg);
@@ -64,105 +74,120 @@ ipc.on("code", (event, arg) => {
     trakt.exchange_code(arg).then(result => {
         nconf.set("TOKEN", trakt.export_token());
         nconf.save();
-        mainWindow.webContents.send("feedback", {text: "Connected to Trakt"});
+        mainWindow.webContents.send("feedback", {
+            text: "Connected to Trakt"
+        });
 
         loginwindows.destroy();
         setTimeout(() => {
-          syncShows();
+            syncShows();
         }, 2000)
     });
 })
 let syncShows = () => {
-   mainWindow.webContents.send("feedback", {text: "Show sync started..."});
+    mainWindow.webContents.send("feedback", {
+        text: "Show sync started..."
+    });
 
-   trakt.sync.watched({
-       type:"shows"
-   }).then((result) => {
-       let shows = [];
-       console.log(result.length);
-       for(let i = 0; i < result.length; i++){
-           shows.push(result[i].show.title);
-       }
+    trakt.sync.watched({
+        type: "shows"
+    }).then((result) => {
+        let shows = [];
+        console.log(result.length);
+        for (let i = 0; i < result.length; i++) {
+            shows.push(result[i].show.title);
+        }
 
-       nconf.set("SHOWS", shows);
-       nconf.set("SYNC", result);
-       nconf.save();
-       setTimeout(()=>{
-         mainWindow.webContents.send("feedback", {text: "Show sync finished"});
+        nconf.set("SHOWS", shows);
+        nconf.set("SYNC", result);
+        nconf.save();
+        setTimeout(() => {
+            mainWindow.webContents.send("feedback", {
+                text: "Show sync finished"
+            });
 
-       }, 2000)
+        }, 2000)
 
-       //console.log(result);
-   }).catch((error) => {
-      mainWindow.webContents.send("feedback", {text: "Show sync failed"});
+        //console.log(result);
+    }).catch((error) => {
+        mainWindow.webContents.send("feedback", {
+            text: "Show sync failed"
+        });
 
-       console.log(error)
-   }).then(() => {
+        console.log(error)
+    }).then(() => {
 
-   })
+    })
 };
 ipc.on("postToTrakt", (event, arg) => {
-  let serie, saison, ep;
-  serie = arg.show;
-  saison = arg.saison;
-  ep = arg.ep;
-  let shows = nconf.get("SHOWS");
-  let sync = nconf.get("SYNC");
+    let serie, saison, ep;
+    serie = arg.show;
+    saison = arg.saison;
+    ep = arg.ep;
+    let shows = nconf.get("SHOWS");
+    let sync = nconf.get("SYNC");
 
-  for(let i = 0; i < sync.length; i++)
-  {
-      if(sync[i].show.title == serie){
-          theShow = sync[i].show;
-          break;
-      }
-  }
+    for (let i = 0; i < sync.length; i++) {
+        if (sync[i].show.title == serie) {
+            theShow = sync[i].show;
+            break;
+        }
+    }
 
-  let seasons = [{
-      "number": parseInt(saison),
-      "episodes":[{
-          "number": ep,
-      }]
-  }];
+    let seasons = [{
+        "number": parseInt(saison),
+        "episodes": [{
+            "number": ep,
+        }]
+    }];
 
-  mainWindow.webContents.send("feedback", {text: "Sending to Trakt..."});
-  theShow["seasons"] = seasons
+    mainWindow.webContents.send("feedback", {
+        text: "Sending to Trakt..."
+    });
+    theShow["seasons"] = seasons
 
 
-  trakt.sync.history.add({
-      episodes : [],
-      movies: [],
-      shows : [theShow]
-  }).then((result) => {
+    trakt.sync.history.add({
+        episodes: [],
+        movies: [],
+        shows: [theShow]
+    }).then((result) => {
 
-      console.log(result)
-      event.sender.send("feedback",{text: "Saved on trakt!"})
+        console.log(result)
+        event.sender.send("feedback", {
+            text: "Saved on trakt!"
+        })
 
-  }).catch((err) => {
-      event.sender.send("feedback",{text:`Error : ${err.message}`})
-  });
+    }).catch((err) => {
+        event.sender.send("feedback", {
+            text: `Error : ${err.message}`
+        })
+    });
 });
-ipc.on("file", (event, arg)=>{
+ipc.on("file", (event, arg) => {
     let shows = nconf.get("SHOWS");
     let sync = nconf.get("SYNC");
     try {
-      let cleanedTitle = path.basename(arg);
-      var regEx = new RegExp("S([0-9]{1,2})E([0-9]{1,2})", "i");
-      var match = cleanedTitle.match(regEx);
-      let sai = match[1];
-      let ep = match[2];
-      cleanedTitle = cleanedTitle.substring(0, match.index);
-      cleanedTitle = cleanedTitle.replace(new RegExp("((\\.|-|\\s)^)", 'g'), "");
+        let cleanedTitle = path.basename(arg);
+        var regEx = new RegExp("S([0-9]{1,2})E([0-9]{1,2})", "i");
+        var match = cleanedTitle.match(regEx);
+        let sai = match[1];
+        let ep = match[2];
+        cleanedTitle = cleanedTitle.substring(0, match.index);
+        cleanedTitle = cleanedTitle.replace(new RegExp("((\\.|-|\\s)^)", 'g'), "");
 
-      let found = didYouMean(cleanedTitle, shows, {
-          returnType: 'all-closest-matches',
-          trimSpace: false,
-          thresholdType: 'edit-distance',
-          threshold: Infinity
-      });
-      mainWindow.webContents.send("found", found, sai, ep);
+        let found = didYouMean(cleanedTitle, shows, {
+            returnType: 'all-closest-matches',
+            trimSpace: false,
+            thresholdType: 'edit-distance',
+            threshold: Infinity
+        });
+        mainWindow.webContents.send("found", found, sai, ep);
 
     } catch (e) {
-      event.sender.send("feedback",{text:`Unable to find any show`})
+        event.sender.send("feedback", {
+            text: `Unable to find any show`
+        })
 
     } finally {
 
@@ -170,7 +195,10 @@ ipc.on("file", (event, arg)=>{
 
 });
 ipc.on("openLoginWindow", () => {
-  loginwindows = new BrowserWindow({width: 500, height: 600});
-  const traktAuthUrl = trakt.get_url();
-  loginwindows.loadURL(traktAuthUrl);
+    loginwindows = new BrowserWindow({
+        width: 500,
+        height: 600
+    });
+    const traktAuthUrl = trakt.get_url();
+    loginwindows.loadURL(traktAuthUrl);
 })
